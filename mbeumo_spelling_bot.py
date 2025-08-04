f2acba495d80de43f1903d5ad0050834b42796c7
Chris Cameron-Mills <chris.cameronmills@gmail.com>
2025-08-04 08:48:22 +0100

    Changed pronounciation after feedback

--- mbeumo_spelling_bot.py
+++ mbeumo_spelling_bot.py
@@ -39,7 +39,7 @@ LIMIT_TO_SUBMISSION_TITLED = os.getenv("LIMIT_TO_SUBMISSION_TITLED")
 REPLY_TEMPLATE = (
     "👋 Just a quick heads‑up — I think you meant **{correct}**, not “{found}”. " \
     "\n\n---\n\n*^([Youtube link](https://youtube.com/shorts/pocySXnRwl8?si=2a0UE1vqdANWHT6Q) "
-    "of Bryan Em-ber-mo saying his name)*"
+    "of Bryan Em-boo-mo (?) saying his name)*"
 )
 
 # ---------------------------------------------------------------------------
